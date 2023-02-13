799ba632e47060bb4f6ed31c3e151c951a3d4a14
Alex A. Rocha <lex99r@gmail.com>
2023-02-13 09:33:50 -0400

    Initial commit

---
+++ CryptedTruth/CryptedTruthApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptedTruthApp.swift
+//  CryptedTruth
+//
+//  Created by Alex A. Rocha on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptedTruthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
