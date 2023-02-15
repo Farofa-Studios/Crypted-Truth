3bd01fbe8ebf6d8b30764ee1145d63f728c2ba20
Victor Luiz <victor_luizsantos@hotmail.com>
2023-02-15 11:13:28 -0400

    MiniGame3 Base
    View and buttons structure, assets for testing

---
+++ CryptedTruth/MiniGame3ButtonInfos.swift
@@ -0,0 +1,13 @@
+//
+//  MiniGame3ButtonInfos.swift
+//  CryptedTruth
+//
+//  Created by Victor Santos on 2/15/23.
+//
+
+import Foundation
+
+struct MiniGame3ButtonInfos {
+    var image: String
+    var rotation: Double
+}
