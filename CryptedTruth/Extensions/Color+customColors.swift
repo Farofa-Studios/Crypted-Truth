239ac2d704220ee68612a79e4a3183b80debae5a
Anna Milani <aavm.snf19@uea.edu.br>
2023-02-23 08:57:21 -0400

    chore: fix conflicts - geniusView

---
+++ CryptedTruth/Extensions/Color+customColors.swift
@@ -0,0 +1,16 @@
+//
+//  Color+customColors.swift
+//  CryptedTruth
+//
+//  Created by Alex A. Rocha on 17/02/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let darkColor = Color("dark-color")
+    static let primaryColor = Color("primary-color")
+    static let secondaryColor = Color("secondary-color")
+    static let accentColor = Color("accent-color")
+}
