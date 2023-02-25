455696ca7242e5a920be4b1ee44469fb86e8fb2a
Kiara Medeiros <kiaramedeiros2001@gmail.com>
2023-02-25 17:45:20 -0400

    [chore]: add challengeConcluded view

--- CryptedTruth/Views/SubtitleView.swift
+++ CryptedTruth/Views/SubtitleView.swift
@@ -11,14 +11,14 @@ struct SubtitleView: View {
     var body: some View {
         HStack (alignment: .center, spacing: 16) {
             
-            Image("person-1")
+            Image("Bot")
                 .resizable()
                 .frame(width: 264, height: 200)
             
             ZStack {
                 Rectangle()
                     .frame(width: 1320, height: 200, alignment: .center)
-                    .foregroundColor(Color.blue)
+                    .foregroundColor(Color(red: 1, green: 1, blue: 1, opacity: 0.1))
                 Text("Hello blablabla")
                     .frame(width: 1255.81, height: 120, alignment: .leading)
             }
