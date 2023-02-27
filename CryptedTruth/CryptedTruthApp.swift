d6e8f8dffa438b6abacea2b4a99028dc0857ca2d
Victor Luiz <victor_luizsantos@hotmail.com>
2023-02-27 12:52:20 -0400

    chore: audio in struct

--- CryptedTruth/CryptedTruthApp.swift
+++ CryptedTruth/CryptedTruthApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct CryptedTruthApp: App {
     var body: some Scene {
         WindowGroup {
-            InitialView()
+//            InitialView()
+            SmellMiniGame()
         }
     }
 }
