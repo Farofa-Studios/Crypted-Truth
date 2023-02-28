e9ce60c7fa856d0edb6c7d36d3b0a6a3a6e33558
Alex A. Rocha <lex99r@gmail.com>
2023-02-28 08:17:33 -0400

    hotfix: fixing genius round counter

--- CryptedTruth/ViewModel/GeniusViewModel.swift
+++ CryptedTruth/ViewModel/GeniusViewModel.swift
@@ -40,7 +40,7 @@ class GeniusViewModel: ObservableObject {
         instruments = [sax, guitar, piano, tambourine]
         
         self.matchInstruments = GeniusViewModel.createGeniusGame(instruments: instruments)
-        self.roundCounter = 3
+        self.roundCounter = 1
         self.mistakesCounter = 0
         self.isPlayerTurn = false
         self.isCorrectInput = nil
