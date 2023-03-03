23268ba6fc4b147c918c22f1d6e136d5167ce0a5
Kiara Medeiros <kiaramedeiros2001@gmail.com>
2023-03-03 11:50:19 -0400

    fix: audio model cardboard

--- CryptedTruth/Services/CardSubtitlesService.swift
+++ CryptedTruth/Services/CardSubtitlesService.swift
@@ -11,13 +11,13 @@ struct CardSubtitle {
     
     static let allICardSubtitles: [Subtitle] = [
         
-        .init(id: 36, image: "sophia-medo", type: .streamer, subtitle: "Não acredito! Vincent Dubois o perfumista sensação nas redes sociais foi encontrado morto em sua própria loja há 20 dias, no dia 08. Meu pai amado! Foi o dia em que eu estava lá escolhendo um presente para uma amiga! A bizarra causa da sua morte foi afogamento em 50 litros de perfume!", audio: "Quadro 19"),
+        .init(id: 36, image: "sophia-medo", type: .streamer, subtitle: "Não acredito! Vincent Dubois o perfumista sensação nas redes sociais foi encontrado morto em sua própria loja há 20 dias, no dia 08. Meu pai amado! Foi o dia em que eu estava lá escolhendo um presente para uma amiga! A bizarra causa da sua morte foi afogamento em 50 litros de perfume!", audio: "Quadro 14"),
         
-            .init(id: 37, image: "sophia-chocada", type: .streamer, subtitle: "Sandra Antonella a chefe de cozinha, … Espera! Esse nome não me estranho! Não pode ser! Ela estava na minha cidade em um festival de culinária, há 15 dias! No dia 13 o dia em que ela morreu! Sandra foi envenenada após comer 5 de seus próprios pratos!", audio: ""),
+            .init(id: 37, image: "sophia-chocada", type: .streamer, subtitle: "Sandra Antonella a chefe de cozinha, … Espera! Esse nome não me estranho! Não pode ser! Ela estava na minha cidade em um festival de culinária, há 15 dias! No dia 13 o dia em que ela morreu! Sandra foi envenenada após comer 5 de seus próprios pratos!", audio: "Quadro 12"),
         
-            .init(id: 38, image: "sophia-normal", type: .streamer, subtitle: "Nossa! Que triste, eu seguia a Ming Yu nas redes sociais, amava as peças de artesanato que ela fazia, e os tutoriais que ela sempre postava eram incríveis! Ela faleceu há apenas 10 dias, no dia 18, que jeito horrível de morrer! Carbonizada no seu próprio forno de cerâmica!", audio: ""),
+            .init(id: 38, image: "sophia-normal", type: .streamer, subtitle: "Nossa! Que triste, eu seguia a Ming Yu nas redes sociais, amava as peças de artesanato que ela fazia, e os tutoriais que ela sempre postava eram incríveis! Ela faleceu há apenas 10 dias, no dia 18, que jeito horrível de morrer! Carbonizada no seu próprio forno de cerâmica!", audio: "Quadro 10"),
             
-            .init(id: 39, image: "sophia-chocada", type: .streamer, subtitle: "Meu Deus, o Saulo Martins morreu? Que horrível, eu estava no show que ele fez no dia 23! O dia em que ele morreu! As autoridades o encontraram morto no carro após ser intoxicado por monóxido de carbono! Eu amava as músicas dele.", audio: "")
+            .init(id: 39, image: "sophia-chocada", type: .streamer, subtitle: "Meu Deus, o Saulo Martins morreu? Que horrível, eu estava no show que ele fez no dia 23! O dia em que ele morreu! As autoridades o encontraram morto no carro após ser intoxicado por monóxido de carbono! Eu amava as músicas dele.", audio: "Quadro 8")
         
     ]
 }
