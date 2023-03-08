8131009252cc2b3d953a60c23f26ae2b39db5103
Victor Luiz <victor_luizsantos@hotmail.com>
2023-03-08 11:16:05 -0400

    feat and fix: boardSubtitle added and fix quadro 4-9-etc name

--- CryptedTruth/Services/BoardSubtitlesService.swift
+++ CryptedTruth/Services/BoardSubtitlesService.swift
@@ -13,16 +13,16 @@ struct BoardSubtitles {
         
         // minigame audição
         
-        .init(id: 26, image: "sophia-normal", type: .streamer, subtitle: "Então vamos lá! Nossa! Parece aqueles quadros de crimes, é esse o nome? Não! Quadro de investigações, isso! Ok, ok, @five estou gostando do seu comprometimento (embora eu ache que a sua vida social deva ser inexistente).", audio: ""),
-        .init(id: 27, image: "sophia-normal", type: .streamer, subtitle: "Bom como nada na vida é fácil, sinto dizer que as informações de nossas: uma, duas, três, quatro vítimas! estão bloqueadas… Vamos escolher uma para tentar desbloquear primeiro.", audio: ""),
-        .init(id: 28, image: "sophia-chocada", type: .streamer, subtitle: "Que isso?! Tem umas letras aqui no meio... Deve ser algum código que tenho que descobrir enquanto desbloqueio as informações... Vamos ver o que desbloqueamos sobre essa vítima.", audio: ""),
-        .init(id: 29, image: "sophia-normal", type: .streamer, subtitle: "Beleza, agora vamos para a próxima...", audio: ""),
-        .init(id: 30, image: "sophia-chocada", type: .streamer, subtitle: "Novas letras apareceram! Vamos ver as informações dessa vítima que desbloqueamos.", audio: ""),
-        .init(id: 31, image: "sophia-concentrada", type: .streamer, subtitle: "Gente... Como assim? Então a morte de cada vítima está ligada a sua profissão de alguma forma?!...", audio: ""),
-        .init(id: 32, image: "sophia-concentrada", type: .streamer, subtitle: "Olhem! Temos todas as letras para descobrir qual é o código ali do meio...", audio: ""),
-        .init(id: 33, image: "sophia-chocada", type: .streamer, subtitle: "A palavra era mesmo “SENTIDOS”! Então cada uma das vítimas está ligada a um sentido do ser humano! Espera aí... mas... tá faltando a VISÃO... que estaria ligado a essa quinta e última vítima que só apareceu agora!", audio: ""),
-        .init(id: 34, image: "sophia-medo", type: .streamer, subtitle: "Mas gente! Fazendo as contas aqui, se a diferença entre as datas das mortes de cada uma das vítimas era de 5 dias... Isso significa que a quinta vítima... morreu 5 dias depois do Saulo... Isso seria hoje!?", audio: ""),
-        .init(id: 35, image: "sophia-medo", type: .streamer, subtitle: "A quinta vítima deve ter uma profissão relacionada com o sentido da visão... Talvez uma fotógrafa, ou um produtor de vídeos... ou... uma... streamer...??", audio: ""),
+        .init(id: 26, image: "sophia-normal", type: .streamer, subtitle: "Então vamos lá! Nossa! Parece aqueles quadros de crimes, é esse o nome? Não! Quadro de investigações, isso! Ok, ok, @five estou gostando do seu comprometimento (embora eu ache que a sua vida social deva ser inexistente).", audio: "Quadro 1"),
+        .init(id: 27, image: "sophia-normal", type: .streamer, subtitle: "Bom como nada na vida é fácil, sinto dizer que as informações de nossas: uma, duas, três, quatro vítimas! estão bloqueadas… Vamos escolher uma para tentar desbloquear primeiro.", audio: "Quadro 2"),
+        .init(id: 28, image: "sophia-chocada", type: .streamer, subtitle: "Que isso?! Tem umas letras aqui no meio... Deve ser algum código que tenho que descobrir enquanto desbloqueio as informações... Vamos ver o que desbloqueamos sobre essa vítima.", audio: "Quadro 3"),
+        .init(id: 29, image: "sophia-normal", type: .streamer, subtitle: "Beleza, agora vamos para a próxima...", audio: "Quadro 4-9-11-13"),
+        .init(id: 30, image: "sophia-chocada", type: .streamer, subtitle: "Novas letras apareceram! Vamos ver as informações dessa vítima que desbloqueamos.", audio: "Quadro 5"),
+        .init(id: 31, image: "sophia-concentrada", type: .streamer, subtitle: "Gente... Como assim? Então a morte de cada vítima está ligada a sua profissão de alguma forma?!...", audio: "Quadro 6"),
+        .init(id: 32, image: "sophia-concentrada", type: .streamer, subtitle: "Olhem! Temos todas as letras para descobrir qual é o código ali do meio...", audio: "Quadro 7"),
+        .init(id: 33, image: "sophia-chocada", type: .streamer, subtitle: "A palavra era mesmo “SENTIDOS”! Então cada uma das vítimas está ligada a um sentido do ser humano! Espera aí... mas... tá faltando a VISÃO... que estaria ligado a essa quinta e última vítima que só apareceu agora!", audio: "Quadro 16"),
+        .init(id: 34, image: "sophia-medo", type: .streamer, subtitle: "Mas gente! Fazendo as contas aqui, se a diferença entre as datas das mortes de cada uma das vítimas era de 5 dias... Isso significa que a quinta vítima... morreu 5 dias depois do Saulo... Isso seria hoje!?", audio: "Quadro 17"),
+        .init(id: 35, image: "sophia-medo", type: .streamer, subtitle: "A quinta vítima deve ter uma profissão relacionada com o sentido da visão... Talvez uma fotógrafa, ou um produtor de vídeos... ou... uma... streamer...??", audio: "Quadro 18"),
         
     ]
 }
